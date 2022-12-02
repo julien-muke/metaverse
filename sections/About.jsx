910dca679f53375b7f410421d4c103c07513a430
julien-muke <julien.muke@gmail.com>
2022-12-03 00:50:19 +0200

    setting the about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,5 +1,11 @@
 'use client';
 
+import { motion } from "framer-motion";
+import { typingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
   <section>
     About section
